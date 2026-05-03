4c5e4dc9e1ea987d2124c68963afb4de93064020
Rajommkar <marthaommkarnath@gmail.com>
2026-05-03 22:34:45 +0530

    Update Translation model schema with user ref

--- backend/models/Translation.js
+++ backend/models/Translation.js
@@ -1,29 +1,12 @@
 const mongoose = require("mongoose");
 
 const translationSchema = new mongoose.Schema({
-  userId: {
+  user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
+    ref: "User"
   },
-  originalText: {
-    type: String,
-    required: true,
-  },
-  translatedText: {
-    type: String,
-    required: true,
-  },
-  sourceLanguage: {
-    type: String,
-    default: "auto",
-  },
-  targetLanguage: {
-    type: String,
-    default: "en",
-  },
+  originalText: String,
+  translatedText: String,
 }, { timestamps: true });
-
-translationSchema.index({ userId: 1, createdAt: -1 });
-translationSchema.index({ originalText: 1 });
 
 module.exports = mongoose.model("Translation", translationSchema);
